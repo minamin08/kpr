ba51ece02c7263c7f076c5015531502ea6f26011
minamin08 <91894801+minamin08@users.noreply.github.com>
2025-07-29 10:23:22 +0700

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,32 @@
+/*  java Script */
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+
+
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+function hitungKPR() {
+  var jumlah_pinjaman = parseFloat(document.getElementById("jumlah_pinjaman").value);
+  var suku_bunga_tahunan = 1.5; // Menggunakan suku bunga tetap (contoh 8%)
+  var tenor_bulan = parseFloat(document.getElementById("tenor").value);
+
+  var suku_bunga_bulanan = suku_bunga_tahunan / 100 ;
+
+  var pembayaran_per_bulan = (jumlah_pinjaman / tenor_bulan) + (jumlah_pinjaman * suku_bunga_bulanan);
+  var formatter = new Intl.NumberFormat('id-ID',{
+    style: 'currency', currency: 'IDR'
+  });
+  
+  var pembayaran_per_bulan_formatted = formatter.format(pembayaran_per_bulan);
+
+  document.getElementById("result").innerHTML = "Angsuran bulanan KPR Anda sebesar:  " + pembayaran_per_bulan_formatted;
+}
