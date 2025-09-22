bcc93fb39516df7f48c88af65f26af093f9004f6
minamin08 <91894801+minamin08@users.noreply.github.com>
2025-09-22 14:20:40 +0700

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,8 +1,21 @@
-/*  java Script */
+/*===== MENU SHOW Y HIDDEN =====*/ 
 const navMenu = document.getElementById('nav-menu'),
       toggleMenu = document.getElementById('nav-toggle'),
       closeMenu = document.getElementById('nav-close')
- 
+
+/*SHOW*/ 
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+/*HIDDEN*/
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
 function linkAction(){
   /*Active link*/
   navLink.forEach(n => n.classList.remove('active'));
