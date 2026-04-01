2e424a35be91662cef7dcac7b81640092a9991f5
minamin08 <91894801+minamin08@users.noreply.github.com>
2026-04-01 14:48:02 +0700

    Update script.js

--- app/script.js
+++ app/script.js
@@ -2,7 +2,27 @@
 const navMenu = document.getElementById('nav-menu'),
       toggleMenu = document.getElementById('nav-toggle'),
       closeMenu = document.getElementById('nav-close')
+/*===== MENU SHOW =====*/ 
+if(toggleMenu){
+    toggleMenu.addEventListener('click', ()=>{
+        navMenu.classList.add('show')
+    })
+}
 
+/*===== MENU HIDDEN =====*/
+if(closeMenu){
+    closeMenu.addEventListener('click', ()=>{
+        navMenu.classList.remove('show')
+    })
+}
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 function linkAction(){
